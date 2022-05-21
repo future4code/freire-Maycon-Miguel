4f972a1e921d62ccb3e041ed5c578376c60cfd06
Maycon Coutinho <mayconcoutinhoart@gmail.com>
2022-05-21 10:58:17 -0300

    blackjack

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -11,3 +11,163 @@
  * 
  */
 
+///////////////////////////// funções ////////////////////////////////////////
+
+// Adicionando as cartas -> Funcionando
+
+let usuarioCartas = []
+let computadorCartas =  []
+
+function adicionandoCarta(jogador, quantidade){
+
+   quantidadeComparativa = 0
+
+   while(quantidadeComparativa != quantidade){
+   if(jogador === "usuario"){
+         usuarioCartas.push(comprarCarta())
+   } 
+   if(jogador === "computador"){
+         computadorCartas.push(comprarCarta())
+      }
+   quantidadeComparativa++
+   }
+}
+// Pontuação dos jogadores
+
+function pontuacaoUsuario(usuarioCartas){
+
+   pontuacao = 0 
+   contador = 0
+   comparador = usuarioCartas.length
+
+   while(comparador != contador){
+      pontuacao = usuarioCartas[contador].valor + pontuacao
+      contador++
+   }
+   return pontuacao
+}
+function pontuacaoComputador(computadorCartas){
+
+   pontuacao = 0 
+   contador = 0
+   comparador = computadorCartas.length
+
+   while(comparador != contador){
+      pontuacao = computadorCartas[contador].valor + pontuacao
+      contador++
+   }
+   return pontuacao
+}
+
+// Conferindo se as duas cartas iniciais do usuário ou do computador forem dois ases (A) e sorteando novamente.
+
+function conferindoAses(pontuacaoUsuario,pontuacaoComputador){
+
+   if (pontuacaoUsuario === 22){
+      while(usuarioCartas.length) {
+         usuarioCartas.pop();
+      }
+      usuarioCartas.push(comprarCarta())
+      usuarioCartas.push(comprarCarta())
+   }
+   if (pontuacaoComputador === 22){
+      while(computadorCartas.length) {
+         computadorCartas.pop();
+      }
+      computadorCartas.push(comprarCarta())
+      computadorCartas.push(comprarCarta())
+   }
+}
+//  Confere ganhador 
+
+function confereGanhador(usuarioPontos, computadorPontos){
+
+   if(usuarioPontos === computadorPontos){
+      alert("Empate!")
+   }
+   if(usuarioPontos <= computadorPontos){
+      alert("O computador ganhou!")
+   }
+   if(usuarioPontos >= computadorPontos){
+      alert("O usuário ganhou!")
+   }
+}
+// Imprimindo resultado e confirmando se quer compra mais cartas
+
+function usuarioCompraMaisCarta(){
+
+   while(true){
+      if(pontuacaoUsuario(usuarioCartas) < 21){
+         
+         let compraMaisCarta = confirm(`Usuário - cartas:  ${usuarioCartas[0].texto} ${usuarioCartas[1].texto}. A carta revelada do computador é ${computadorCartas[0].texto}.
+         Deseja compra mais uma carta? `)
+      
+         if(compraMaisCarta === true){
+            usuarioCartas.push(comprarCarta())
+         }
+         if(compraMaisCarta === false){ break } 
+      }
+
+      if(pontuacaoUsuario(usuarioCartas) >= 21){
+         break
+         } 
+   }
+}
+
+// Zera o jogo
+
+function zeraJogo(usuarioCartas,computadorCartas){
+
+      while(usuarioCartas.length) {
+         usuarioCartas.pop();
+      }
+      while(computadorCartas.length) {
+         computadorCartas.pop();
+      }
+   }
+
+
+///////////////////////////////  Boas vindas  //////////////////////////////////////////
+function blackjack(){
+
+   alert("Boas vindas ao jogo de Blackjack!")
+
+   // Confirmação se quer jogar
+
+   const iniciarJogo = confirm("Quer iniciar uma nova rodada?") 
+
+   // Caso não queire jogar o jogo acaba
+
+   if(iniciarJogo === false){
+      alert("O jogo acabou")
+   }
+
+   // Inicio do jogo
+
+   if(iniciarJogo === true){
+
+      // Comprando cartas 
+
+      adicionandoCarta("usuario", 2)
+      adicionandoCarta("computador", 2)
+
+      // Conferindo ases
+      
+      conferindoAses(pontuacaoUsuario(usuarioCartas),pontuacaoComputador(computadorCartas))
+      
+      // Decidir se quer compra mais cartas
+
+      usuarioCompraMaisCarta()
+
+      confereGanhador(pontuacaoUsuario(usuarioCartas), pontuacaoComputador(computadorCartas))
+
+      let jogarNovamente = confirm("Deseja jogar novamente? ")
+
+      if(jogarNovamente === true){
+         zeraJogo(usuarioCartas,computadorCartas)
+         blackjack()
+      }
+   }
+}
+
+blackjack()
--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -10,3 +10,55 @@
  * 
  * 
  */
+
+
+// 1º Etapa
+
+console.log("Boas vindas ao jogo de Blackjack!")
+
+
+// 2º Etapa
+
+let iniciarJogo = Number(prompt("Quer iniciar uma nova rodada? [1] sim , [2] Não")) === 1
+
+// 3º Etapa
+
+if(iniciarJogo === false){
+   console.log("O jogo acabou")
+}
+
+// 4º Etapa
+
+if(iniciarJogo === true){
+
+// 5º Etapa  
+
+   let usuarioCartas = []
+   let computadorCartas =  []
+
+   usuarioCartas.push(comprarCarta())
+   usuarioCartas.push(comprarCarta())
+
+   computadorCartas.push(comprarCarta())
+   computadorCartas.push(comprarCarta())
+
+// 6º Etapa
+
+   const pontuacaoUsuario = usuarioCartas[0].valor + usuarioCartas[1].valor
+   const pontuacaoComputador = computadorCartas[0].valor + computadorCartas[1].valor
+  
+   console.log(`Usuário - cartas:  ${usuarioCartas[0].texto} ${usuarioCartas[1].texto}   - pontuação ${pontuacaoUsuario} `)
+   console.log(`Computador - cartas:  ${computadorCartas[0].texto} ${computadorCartas[1].texto}   - pontuação ${pontuacaoComputador} `)
+
+// 7º Etapa
+
+   if(pontuacaoUsuario === pontuacaoComputador){
+      console.log("Empate!")
+   }
+   if(pontuacaoUsuario <= pontucaoComputador){
+      console.log("O computador ganhou!")
+   }
+   if(pontuacaoUsuario >= pontuacaoComputador){
+      console.log("O usuário ganhou!")
+   }
+}
