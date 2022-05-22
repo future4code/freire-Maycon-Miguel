6d82b5c4769c290d4ec146530eed2d63db1328ae
Maycon Coutinho <mayconcoutinhoart@gmail.com>
2022-05-22 10:44:21 -0300

    projeto-blackjack-finalizado

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -11,34 +11,34 @@
  * 
  */
 
-///////////////////////////// funções ////////////////////////////////////////
+////////////////////////////////////////// Principais Funções //////////////////////////////////////////////////////
 
-// Adicionando as cartas -> Funcionando
-
+// Array com todas as cartas dos jogadores
 let usuarioCartas = []
 let computadorCartas =  []
 
+// Entregando as cartas aos jogadores 
 function adicionandoCarta(jogador, quantidade){
 
-   quantidadeComparativa = 0
+   let quantidadeComparativa = 0
 
    while(quantidadeComparativa != quantidade){
-   if(jogador === "usuario"){
+      if(jogador === "usuario"){
          usuarioCartas.push(comprarCarta())
-   } 
-   if(jogador === "computador"){
-         computadorCartas.push(comprarCarta())
+      } 
+      if(jogador === "computador"){
+      computadorCartas.push(comprarCarta())
       }
-   quantidadeComparativa++
+      quantidadeComparativa++
    }
 }
-// Pontuação dos jogadores
 
+// Somando e retornando a pontuação do usuario
 function pontuacaoUsuario(usuarioCartas){
 
-   pontuacao = 0 
-   contador = 0
-   comparador = usuarioCartas.length
+   let pontuacao = 0 
+   let contador = 0
+   const comparador = usuarioCartas.length
 
    while(comparador != contador){
       pontuacao = usuarioCartas[contador].valor + pontuacao
@@ -46,11 +46,13 @@ function pontuacaoUsuario(usuarioCartas){
    }
    return pontuacao
 }
+
+// Somando e retornando a pontuação do computador
 function pontuacaoComputador(computadorCartas){
 
-   pontuacao = 0 
-   contador = 0
-   comparador = computadorCartas.length
+   let pontuacao = 0 
+   let contador = 0
+   const comparador = computadorCartas.length
 
    while(comparador != contador){
       pontuacao = computadorCartas[contador].valor + pontuacao
@@ -59,8 +61,7 @@ function pontuacaoComputador(computadorCartas){
    return pontuacao
 }
 
-// Conferindo se as duas cartas iniciais do usuário ou do computador forem dois ases (A) e sorteando novamente.
-
+// Conferindo se as duas cartas iniciais do usuário ou do computador são dois ases (A) e sorteando novamente.
 function conferindoAses(pontuacaoUsuario,pontuacaoComputador){
 
    if (pontuacaoUsuario === 22){
@@ -78,89 +79,154 @@ function conferindoAses(pontuacaoUsuario,pontuacaoComputador){
       computadorCartas.push(comprarCarta())
    }
 }
-//  Confere ganhador 
 
+// Jogadas do computador
+function computadorJogadas(){
+   while(true){
+   if(pontuacaoComputador(computadorCartas) < 21){
+      computadorCartas.push(comprarCarta())
+      } else{break}
+   }
+}
+
+// Confere quem ganhou o jogo 
 function confereGanhador(usuarioPontos, computadorPontos){
 
    if(usuarioPontos === computadorPontos){
-      alert("Empate!")
+      return "Empate!"
    }
-   if(usuarioPontos <= computadorPontos){
-      alert("O computador ganhou!")
+   if(usuarioPontos < 21){
+      if(computadorPontos > 21){
+         return "O usuário ganhou!"
+      }
    }
-   if(usuarioPontos >= computadorPontos){
-      alert("O usuário ganhou!")
+   if(usuarioPontos > 21){
+      return "O computador ganhou!"
+   }
+   if(computadorPontos > 21 && pontuacaoUsuario < computadorPontos){
+      return "O usuário ganhou!"
    }
 }
-// Imprimindo resultado e confirmando se quer compra mais cartas
 
+// Imprimindo resultado das cartas do jogador e confirmando se quer compra mais cartas ou para de compra
 function usuarioCompraMaisCarta(){
 
    while(true){
       if(pontuacaoUsuario(usuarioCartas) < 21){
-         
-         let compraMaisCarta = confirm(`Usuário - cartas:  ${usuarioCartas[0].texto} ${usuarioCartas[1].texto}. A carta revelada do computador é ${computadorCartas[0].texto}.
-         Deseja compra mais uma carta? `)
+
+         let compraMaisCarta = confirm(mensagemDeCartasRevelada("incompleto"))
       
          if(compraMaisCarta === true){
             usuarioCartas.push(comprarCarta())
+            mensagemDeCartasRevelada("incompleto")
          }
          if(compraMaisCarta === false){ break } 
       }
-
       if(pontuacaoUsuario(usuarioCartas) >= 21){
          break
-         } 
+      } 
    }
 }
 
-// Zera o jogo
+// Retorna mensagem monstrando cartas do usuario e cartas do computador
+function mensagemDeCartasRevelada(incompletoOuCompleto){
 
+   if(incompletoOuCompleto === "incompleto"){
+      return `Usuário - cartas:${cartasSorteadasString("usuario")}. A carta revelada do computador é ${computadorCartas[0].texto}.
+Deseja compra mais uma carta? `
+   }
+   if(incompletoOuCompleto === "completo"){
+      return `Usuário - cartas:${cartasSorteadasString("usuario")}. A carta revelada do computador é ${cartasSorteadasString("computador")}.
+Deseja compra mais uma carta? `
+   }
+}
+
+// Alerta, mensagem do Resultado final do jogo
+function mensagemResultadosFinais(){
+
+   alert(`Suas cartas são${cartasSorteadasString("usuario")}. Sua pontuação é ${pontuacaoUsuario(usuarioCartas)}.
+As cartas do computador são${cartasSorteadasString("computador")}. A pontuação do computador é ${pontuacaoComputador(computadorCartas)}. 
+${(confereGanhador(pontuacaoUsuario(usuarioCartas), pontuacaoComputador(computadorCartas)))}`)
+}
+
+// Monstra todas cartas sorteadas em string
+function cartasSorteadasString(usuarioOuComputador){
+
+   if (usuarioOuComputador === "usuario"){
+
+      const comparador = usuarioCartas.length
+      let contador = 0
+      let resultado = " "
+      
+      while(true){
+
+         if(contador === comparador){break}
+
+         resultado = resultado + (" " + usuarioCartas[contador].texto)
+         contador++
+      }
+      resultado = resultado + " "
+      return resultado
+   }
+
+   if (usuarioOuComputador === "computador"){
+
+      const comparador = computadorCartas.length
+      let contador = 0
+
+      let resultado = " "
+      
+      while(true){
+         
+         if(contador === comparador){break}
+         resultado = resultado + " " + computadorCartas[contador].texto
+         contador++
+      }
+      resultado = resultado + " "
+      return resultado
+   }
+}
+
+// Zera o jogo 
 function zeraJogo(usuarioCartas,computadorCartas){
 
       while(usuarioCartas.length) {
          usuarioCartas.pop();
       }
+
       while(computadorCartas.length) {
          computadorCartas.pop();
       }
    }
 
-
-///////////////////////////////  Boas vindas  //////////////////////////////////////////
+///////////////////////////////////////  Boas vindas  ///////////////////////////////////////////////
 function blackjack(){
 
    alert("Boas vindas ao jogo de Blackjack!")
 
    // Confirmação se quer jogar
-
    const iniciarJogo = confirm("Quer iniciar uma nova rodada?") 
 
-   // Caso não queire jogar o jogo acaba
-
-   if(iniciarJogo === false){
-      alert("O jogo acabou")
-   }
-
    // Inicio do jogo
-
    if(iniciarJogo === true){
 
       // Comprando cartas 
-
       adicionandoCarta("usuario", 2)
       adicionandoCarta("computador", 2)
 
-      // Conferindo ases
-      
+      // Conferindo ases  
       conferindoAses(pontuacaoUsuario(usuarioCartas),pontuacaoComputador(computadorCartas))
       
-      // Decidir se quer compra mais cartas
-
+      // Decidir se quer compra mais cartas, jogadas do usuario.
       usuarioCompraMaisCarta()
 
-      confereGanhador(pontuacaoUsuario(usuarioCartas), pontuacaoComputador(computadorCartas))
+      // Computador jogando, jogadas do computador
+      computadorJogadas()
 
+      // Conferindo resultados e anunciando ganhador 
+      mensagemResultadosFinais()
+
+      // Jogar novamente zerando o jogo ou sair do jogo 
       let jogarNovamente = confirm("Deseja jogar novamente? ")
 
       if(jogarNovamente === true){
@@ -171,3 +237,6 @@ function blackjack(){
 }
 
 blackjack()
+alert("O jogo acabou")
+
+
